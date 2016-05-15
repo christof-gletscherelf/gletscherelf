08bf021316b743538cff56fdf0d935af89cbb38e
Christof Meyer <christof.meyer@anivo.ch>
2016-05-15 17:31:33 +0200

    add js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,84 @@
-console.log('This would be the main JS file.');
+window.onload = function() {
+    // create a heatmap instance
+    var heatmap = h337.create({
+      container: document.getElementById('heatmapContainer'),
+      maxOpacity: 0.6,
+      radius: 50,
+      blur: 0.9,
+    });
+    var heatmapContainer = document.getElementById('heatmapContainerWrapper');
+
+    var dragSlider = document.getElementById('drag');
+
+    var img = document.getElementById('footballfield');
+    var fieldWidth = img.clientWidth;
+    var fieldHeight = img.clientHeight;
+
+    noUiSlider.create(dragSlider, {
+      start: [10, 20],
+      behaviour: 'drag',
+      connect: true,
+      range: {
+        'min': 0,
+        'max': 90
+      },
+      format: wNumb({
+        decimals: 0,
+      })
+    });
+
+    var fromMinute = document.getElementById('fromMinute');
+    var toMinute = document.getElementById('toMinute');
+
+    var wholeGameCoordinates = [];
+    wholeGameCoordinates = generateWholeGameData(5400);
+
+    function deviateCoordinateFromLen(len, max) {
+      var random = Math.random();
+      var linearPart = (5400 - len);
+      var randomPart = max * random;
+      var quota = len / max;
+      var normalizedLinearPart = linearPart / quota / 2;
+      var normalizedRandomPart = randomPart / quota / 2;
+      var result = (normalizedLinearPart * 0.8) + (normalizedRandomPart * 0.2);
+      return result >> 0;
+    }
+
+    function generateWholeGameData(len) {
+      var max = 1;
+      var min = 1;
+      var maxX = fieldWidth;
+      var maxY = fieldHeight;
+      var data = [];
+      while (len--) {
+        data.push({
+          x: deviateCoordinateFromLen(len, maxX),
+          y: deviateCoordinateFromLen(len, maxY),
+          value: 1
+        });
+      }
+      return {
+        max: max,
+        min: min,
+        data: data
+      };
+    }
+
+    function getPartialGameCoordinates(from, to) {
+      var partialGameCoordinates = {};
+      partialGameCoordinates.max = wholeGameCoordinates.max;
+      partialGameCoordinates.min = wholeGameCoordinates.min;
+      partialGameCoordinates.data = wholeGameCoordinates.data.slice(from * 60, to * 60);
+      return partialGameCoordinates;
+    }
+
+    dragSlider.noUiSlider.on('update', function(values, handle) {
+      fromMinute.value = values[0];
+      toMinute.value = values[1];
+      heatmap.setData(getPartialGameCoordinates(fromMinute.value, toMinute.value));
+      heatmap.repaint();
+
+    });
+
+
+  };
